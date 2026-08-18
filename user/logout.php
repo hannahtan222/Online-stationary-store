65d2792f4a50f40260f88cb1721c94dc860f88a5
yyyyyyyq12 <yqyq0412@gmail.com>
2026-08-18 20:28:55 +0800

    Update logout.php

--- user/logout.php
+++ user/logout.php
@@ -1,27 +1,30 @@
 <?php
 
-require '../config/db_connection.php';
+session_start();
 
+require_once('../config/db_connection.php');
 
 // ==================================================
 // LOGOUT
 // ==================================================
 
-// Clear all session data
+// Clear all session variables.
 $_SESSION = [];
 
-
-// Destroy current session
+// Destroy the current session.
 session_destroy();
 
-
-// Start a new session so that we can show
-// a logout confirmation message.
+// Start a new session so that we can
+// display the logout confirmation message.
 session_start();
 
+// Display success message.
 flash(
     'success',
     'You have been logged out.'
 );
 
+// Redirect user to login page.
 redirect('login.php');
+
+?>
